1572da0d9ccf985982354f84a4a746cb7121abf6
NTH3C <147485340+NTH3C@users.noreply.github.com>
2024-02-19 16:12:57 +0100

    cgu

---
+++ ldp.js
@@ -0,0 +1,7 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+gsap.registerPlugin(ScrollTrigger) 
+
+gsap.from(".scrollImage1", { scale: 0, duration: 2 });
